ee6ca34bb693e229101c5e984deb32289492e025
Yao Wang <yaowangm@outlook.com>
2025-08-23 16:58:47 +0800

    fix: 修正博客导航卡片中RSS链接路径从/api/projects/改为/api/blog/

--- src/static/js/components/blog-navigation-card.js
+++ src/static/js/components/blog-navigation-card.js
@@ -227,7 +227,7 @@ class BlogNavigationCard extends BaseComponent {
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="/api/projects/${this.projectId}/rss" class="nav-link rss-link" target="_blank">
+                        <a href="/api/blog/${this.projectId}/rss" class="nav-link rss-link" target="_blank">
                             <svg class="link-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                                 <path d="M4 11a9 9 0 0 1 9 9"/>
                                 <path d="M4 4a16 16 0 0 1 16 16"/>
