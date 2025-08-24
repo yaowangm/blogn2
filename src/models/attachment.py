f0fae355646c731c186f9eb9d138e824554a76c5
Yao Wang <yaowangm@outlook.com>
2025-08-24 23:06:23 +0800

    修复SQLAlchemy警告：将declarative_base从sqlalchemy.ext.declarative迁移到sqlalchemy.orm，消除MovedIn20Warning

--- src/models/attachment.py
+++ src/models/attachment.py
@@ -5,7 +5,7 @@
 """
 
 from sqlalchemy import Column, Integer, String, DateTime, BigInteger
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
